6b94b3649e1d27a721f62e84e2bc3b11d0f744bf
Lucas Rodrigues <contato.luccarodrigues@gmail.com>
2021-11-11 11:55:20 -0300

    add validation erros

--- src/Controllers/RepositoriesController.js
+++ src/Controllers/RepositoriesController.js
@@ -1,10 +1,32 @@
 const githubAPI = require("../Services/githubAPI");
 
 async function listAllRepositoriesByOrg(req, res) {
-  const { organization } = req.params;
-  const response = await githubAPI.get(`/orgs/${organization}/repos`);
+  try {
+    const { organization } = req.params;
+    const response = await githubAPI.get(`/orgs/${organization}/repos`);
 
-  return res.status(200).json(response?.data);
+    const { login, id, avatar_url } = response.data[0].owner;
+
+    const organizationData = { login, id, avatar_url };
+
+    const formattedRepositories = [];
+
+    for (const repository of data) {
+      formattedRepositories.push({
+        id: repository?.id,
+        name: repository?.name,
+        private: repository?.private,
+        url: repository?.url,
+        created_at: repository?.created_at,
+        language: repository?.language,
+      });
+    }
+    return res
+      .status(200)
+      .json({ org: organizationData, repos: formattedRepositories });
+  } catch (error) {
+    return res.status(400).json({ message: "Falha ao listar repositórios" });
+  }
 }
 
 module.exports = { listAllRepositoriesByOrg };
